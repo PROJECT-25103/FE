695b91cffd63f62df05b56c4c3bac901eb562376
QuocDL <quoclcph18659@gmail.com>
2025-11-10 10:10:26 +0700

    CREATE MOVIE

--- src/common/services/movie.service.js
+++ src/common/services/movie.service.js
@@ -9,3 +9,8 @@ export const updateStatusMovie = async (id) => {
   const { data } = await api.patch(`/movie/status/${id}`);
   return data;
 };
+
+export const createMovieAPI = async (payload) => {
+  const { data } = await api.post("/movie", payload);
+  return data;
+};
