f93f2f569be040b017760228ae4efb6a3f8ddb7a
trductai <trductai1425@gmail.com>
2025-11-13 08:28:21 +0700

    Giao diện Login

--- src/common/services/auth.service.js
+++ src/common/services/auth.service.js
@@ -4,3 +4,7 @@ export const registerService = async (payload) => {
   const { data } = await api.post("/auth/register", payload);
   return data;
 };
+export const loginService = async (payload)=>{
+  const {data}= await api.post("/auth/login", payload);
+  return data;
+};
